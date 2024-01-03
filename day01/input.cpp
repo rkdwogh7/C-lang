3f3fa32d17c0d19c956193c99affba1f8adad48e
rkdwogh7 <155497579+rkdwogh7@users.noreply.github.com>
2024-01-03 10:48:57 +0900

    Add files via upload

---
+++ day01/input.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+	//º¯¼ö(variable)
+	int mega = 123;
+	printf("%d", mega);
+
+
+
+
+
+	return 0;
+}
---
+++ day01/output.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+	printf("강재호,22,전자전기공학");
+	return 0;
+}
